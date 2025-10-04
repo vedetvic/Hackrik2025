a32d2bdf82990b2fec539c2cfbf9225993dc27ce
Karel Pohl <pohl@smarthorse.cz>
2025-10-04 17:02:37 +0200

    Prvni objektova verze

---
+++ ConsoleHadani/ConsoleHadani/HraHadani.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHadani
+{
+    /// <summary>
+    /// Trida pro hru Hadani cisel
+    /// </summary>
+    public class HraHadani
+    {
+        /// <summary>
+        /// Spusti a zahraje jednu hru Hadani cisel
+        /// </summary>
+        public void Hraj()
+        {
+            Console.Clear();   //smaze obrazovku
+            Console.WriteLine("Vitejte ve hre Hadani cisel!"); //vypise uvodni zpravu
+            Console.WriteLine("Myslim si cislo od 1 do 100. Uhodni ho na co nejmene pokusu.");
+            Random random = new Random(); //vytvori novy objekt pro generovani nahodnych cisel
+            int cislo = random.Next(1, 101);  //vygeneruje nahodne cislo mezi 1 a 100
+            int pokus = 0;  //pocitadlo pokusu
+            int tip = 0;  //uzivateluv tip
+            do
+            {                                           //zacatek smycky (cyklu) do-while
+                Console.Write("Zadejte svuj tip: ");    //vyzve uzivatele k zadani tipu
+                string? vstup = Console.ReadLine();     //nacte uzivateluv vstup
+                try                                     //zachyti vyjimky pri prevodu vstupu na cele cislo
+                {
+                    if (vstup == null)                  //kontrola, zda uzivatel nezadal prazdny vstup
+                    {
+                        Console.WriteLine("Neplatny vstup. Zadejte cele cislo mezi 1 a 100.");  //vypise chybovou zpravu
+                        continue;                       //pokračuje na začátek smyčky
+                    }
+
+                    tip = int.Parse(vstup);             //prevod vstupu na cele cislo
+
+                    if (tip < 1 || tip > 100)           //kontrola, zda je tip v povolenem rozsahu
+                    {
+                        Console.WriteLine("Cislo musi byt mezi 1 a 100. Zkuste to znovu."); //vypise chybovou zpravu
+                        continue;                       //pokračuje na začátek smyčky
+                    }
+                    pokus++;                            //pocita pokusy v pripadě platného tipu
+                }
+                catch                                   //zachyti vyjimky pri prevodu vstupu na cele cislo         
+                {
+                    Console.WriteLine("Neplatny vstup. Zadejte cele cislo mezi 1 a 100."); //vypise chybovou zpravu
+                    continue;                           //pokračuje na začátek smyčky
+                }
+                if (tip < cislo)                        //porovna uzivateluv tip s generovanym cislem
+                {
+                    Console.WriteLine("Moje cislo je vetsi. Zkuste to znovu.");  //vypise napovedu
+                }
+                else if (tip > cislo)                   //porovna uzivateluv tip s generovanym cislem
+                {
+                    Console.WriteLine("Moje cislo je mensi. Zkuste to znovu.");  //vypise napovedu
+                }
+                else
+                {
+                    Console.WriteLine($"Gratuluji! Uhodl jste cislo {cislo} na {pokus} pokusu.");  //vypise gratulaci pri uhodnuti
+
+                }
+
+            } while (tip != cislo);                       //konec smycky (cyklu) do-while, pokracuje dokud uzivatel neuhodne cislo
+        }
+    }
+}
--- ConsoleHadani/ConsoleHadani/Program.cs
+++ ConsoleHadani/ConsoleHadani/Program.cs
@@ -1,53 +1,12 @@
-﻿//Hra hadani cisel
+﻿//Hra hadani cisel - prvni objektova verze
 
-Console.Clear();   //smaze obrazovku
-Console.WriteLine("Vitejte ve hre Hadani cisel!"); //vypise uvodni zpravu
-Console.WriteLine("Myslim si cislo od 1 do 100. Uhodni ho na co nejmene pokusu.");
-Random random = new Random(); //vytvori novy objekt pro generovani nahodnych cisel
-int cislo = random.Next(1, 101);  //vygeneruje nahodne cislo mezi 1 a 100
-int pokus = 0;  //pocitadlo pokusu
-int tip = 0;  //uzivateluv tip
-do
-{                                           //zacatek smycky (cyklu) do-while
-    Console.Write("Zadejte svuj tip: ");    //vyzve uzivatele k zadani tipu
-    string? vstup = Console.ReadLine();     //nacte uzivateluv vstup
-    try                                     //zachyti vyjimky pri prevodu vstupu na cele cislo
-    {
-        if (vstup == null)                  //kontrola, zda uzivatel nezadal prazdny vstup
-        {
-            Console.WriteLine("Neplatny vstup. Zadejte cele cislo mezi 1 a 100.");  //vypise chybovou zpravu
-            continue;                       //pokračuje na začátek smyčky
-        }
+using ConsoleHadani;
 
-        tip = int.Parse(vstup);             //prevod vstupu na cele cislo
+HraHadani mojeHra = new HraHadani(); //vytvori novy objekt tridy HraHadani
+mojeHra.Hraj(); //spusti hru
 
-        if (tip < 1 || tip > 100)           //kontrola, zda je tip v povolenem rozsahu
-        {
-            Console.WriteLine("Cislo musi byt mezi 1 a 100. Zkuste to znovu."); //vypise chybovou zpravu
-            continue;                       //pokračuje na začátek smyčky
-        }
-        pokus++;                            //pocita pokusy v pripadě platného tipu
-    }
-    catch                                   //zachyti vyjimky pri prevodu vstupu na cele cislo         
-    {
-        Console.WriteLine("Neplatny vstup. Zadejte cele cislo mezi 1 a 100."); //vypise chybovou zpravu
-        continue;                           //pokračuje na začátek smyčky
-    }
-    if (tip < cislo)                        //porovna uzivateluv tip s generovanym cislem
-    {
-        Console.WriteLine("Moje cislo je vetsi. Zkuste to znovu.");  //vypise napovedu
-    }
-    else if (tip > cislo)                   //porovna uzivateluv tip s generovanym cislem
-    {
-        Console.WriteLine("Moje cislo je mensi. Zkuste to znovu.");  //vypise napovedu
-    }
-    else
-    {
-        Console.WriteLine($"Gratuluji! Uhodl jste cislo {cislo} na {pokus} pokusu.");  //vypise gratulaci pri uhodnuti
-
-    }
-
-} while (tip!=cislo);                       //konec smycky (cyklu) do-while, pokracuje dokud uzivatel neuhodne cislo
+Console.WriteLine("Dekuji za hru! Stisknete klavesu pro ukonceni."); //vypise dekovnou zpravu pri ukonceni
+Console.ReadKey(); //pocka na stisk klavesy pred ukoncenim programu
 
 
 
